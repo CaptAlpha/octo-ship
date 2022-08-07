2ad65d0ee9aa7f6adf80ddd6e14c664def8da717
CaptAlpha <arhitbose.tagore2020@vitstudent.ac.in>
2022-08-07 18:20:08 +0530

    uploaded render images

--- YOLO-v3-Object-Detection/app.py
+++ YOLO-v3-Object-Detection/app.py
@@ -1,10 +1,9 @@
 from flask import Flask,jsonify,request,render_template,redirect
 import cv2
-from framesplit import FrameCapture
 from yolo_detection_video import detectObj
 from yolo_detection_images import detectObjects
 app = Flask(__name__)
-
+key = ""
 @app.route('/',methods=["POST", "GET"])
 def index():
     if request.method=="POST":
@@ -15,18 +14,37 @@ def index():
 @app.route('/upload',methods=["POST", "GET"])
 def upload():
     if request.method=="POST":
-        f = request.files['file']
-        f.save("video/vid.mp4")
+        key =""
+        if request.files['image']:
+            print("tt")
+            f = request.files['image']
+            f.save("frames/frame.jpg")
+            detectObjects("frames/frame.jpg")
+            key="image"
+            return redirect("/myapp/detectObjectsimg")
+        if request.files['file']:
+            f = request.files['file']
+            f.save("video/vid.mp4")
 
-        detectObj("video/vid.mp4")
-        import moviepy.editor as moviepy
-        clip = moviepy.VideoFileClip("static/chase_output.avi")
-        clip.write_videofile("static/chase_output.mp4")
-        return redirect("/myapp/detectObjects")
-    return render_template("upload.html")
+            detectObj("video/vid.mp4")
+            import moviepy.editor as moviepy
+            clip = moviepy.VideoFileClip("static/chase_output.avi")
+            clip.write_videofile("static/chase_output.mp4")
+            key="video"
+            return redirect("/myapp/detectObjectsvid")
+        
+        
 
-@app.route('/myapp/detectObjects', methods=["POST", "GET"])
+
+
+
+    return render_template("upload_new.html")
+
+@app.route('/myapp/detectObjectsvid', methods=["POST", "GET"])
 def detect():
-    return render_template("render.html")
+    return render_template("render_video.html")
+@app.route('/myapp/detectObjectsimg', methods=["POST", "GET"])
+def detect_img():
+    return render_template("render_image.html")
 if __name__ == "__main__":
     app.run()
--- YOLO-v3-Object-Detection/yolo_detection_images.py
+++ YOLO-v3-Object-Detection/yolo_detection_images.py
@@ -1,65 +1,64 @@
 import numpy as np
 import cv2
 
-confidenceThreshold = 0.5
-NMSThreshold = 0.3
+def detectObjects(image_path):
+    confidenceThreshold = 0.5
+    NMSThreshold = 0.3
 
-modelConfiguration = 'cfg/yolov3.cfg'
-modelWeights = 'yolov3.weights'
+    modelConfiguration = 'cfg/yolov3.cfg'
+    modelWeights = 'yolov3.weights'
 
-labelsPath = 'coco.names'
-labels = open(labelsPath).read().strip().split('\n')
+    labelsPath = 'coco.names'
+    labels = open(labelsPath).read().strip().split('\n')
 
-np.random.seed(10)
-COLORS = np.random.randint(0, 255, size=(len(labels), 3), dtype="uint8")
-COLORS[8] = [0,0,255]
+    np.random.seed(10)
+    COLORS = np.random.randint(0, 255, size=(len(labels), 3), dtype="uint8")
+    COLORS[8] = [0,0,255]
 
-net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+    net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 
-image = cv2.imread('images/boat1.jpg')
-(H, W) = image.shape[:2]
+    image = cv2.imread(image_path)
+    (H, W) = image.shape[:2]
 
-#Determine output layer names
-layerName = net.getLayerNames()
-layerName = [layerName[i-1] for i in net.getUnconnectedOutLayers()]
+    #Determine output layer names
+    layerName = net.getLayerNames()
+    layerName = [layerName[i[0]-1] for i in net.getUnconnectedOutLayers()]
 
-blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB = True, crop = False)
-net.setInput(blob)
-layersOutputs = net.forward(layerName)
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB = True, crop = False)
+    net.setInput(blob)
+    layersOutputs = net.forward(layerName)
 
-boxes = []
-confidences = []
-classIDs = []
+    boxes = []
+    confidences = []
+    classIDs = []
 
-for output in layersOutputs:
-    for detection in output:
-        scores = detection[5:]
-        classID = np.argmax(scores)
-        confidence = scores[classID]
-        if confidence > confidenceThreshold:
-            box = detection[0:4] * np.array([W, H, W, H])
-            (centerX, centerY,  width, height) = box.astype('int')
-            x = int(centerX - (width/2))
-            y = int(centerY - (height/2))
+    for output in layersOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if confidence > confidenceThreshold:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY,  width, height) = box.astype('int')
+                x = int(centerX - (width/2))
+                y = int(centerY - (height/2))
 
-            boxes.append([x, y, int(width), int(height)])
-            confidences.append(float(confidence))
-            classIDs.append(classID)
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
 
-#Apply Non Maxima Suppression
-detectionNMS = cv2.dnn.NMSBoxes(boxes, confidences, confidenceThreshold, NMSThreshold)
+    #Apply Non Maxima Suppression
+    detectionNMS = cv2.dnn.NMSBoxes(boxes, confidences, confidenceThreshold, NMSThreshold)
 
-if(len(detectionNMS) > 0):
-    for i in detectionNMS.flatten():
-        (x, y) = (boxes[i][0], boxes[i][1])
-        (w, h) = (boxes[i][2], boxes[i][3])
+    if(len(detectionNMS) > 0):
+        for i in detectionNMS.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
 
-        color = [int(c) for c in COLORS[classIDs[i]]]
-        text = '{}: {:.4f}'.format(labels[classIDs[i]], confidences[i])
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            text = '{}: {:.4f}'.format(labels[classIDs[i]], confidences[i])
 
-        if 'boat' in text:
-            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-cv2.imshow('Image', image)
-cv2.waitKey(0)
+            if 'boat' in text:
+                cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    cv2.imwrite("static/img.png", image)
