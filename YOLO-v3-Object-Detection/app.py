4b7abb19d6f898e0cfe0935fe8ab3df067d254ca
unknown <virajit.vinaydutt2021@vitstudent.ac.in>
2022-08-07 12:19:58 +0530

    Added YOLO model with flask

--- YOLO-v3-Object-Detection/app.py
+++ YOLO-v3-Object-Detection/app.py
@@ -1,12 +1,22 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template,redirect
 from yolo_detection_images import detectObjects
 app = Flask(__name__)
 
-@app.route('/myapp/detectObjects')
+@app.route('/',methods=["POST", "GET"])
+def hello():
+    if request.method=="POST":
+        return redirect("/myapp/detectObjects")
+    
+    return render_template("index.html")
+
+@app.route('/myapp/detectObjects', methods=["POST", "GET"])
 def detect():
     img = request.args['image']
     img_path = 'images/'+img
     results = detectObjects(img_path)
-    return jsonify(results)
 
-app.run()
+    print(results)
+
+    return jsonify(results)
+if __name__ == "__main__":
+    app.run()
